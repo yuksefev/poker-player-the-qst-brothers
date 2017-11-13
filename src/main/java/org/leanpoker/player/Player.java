af3dfb65dda73cca6fd2148edf596570f1a9dbb2
FYuekseldi <FYuekseldi@ADC-NB10845.intern.cube.ch>
2017-11-13 13:54:19 +0100

    Ultra complex bet strategy

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,7 +1,9 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
+import java.math.BigInteger;
 import java.util.Map;
 
 public class Player {
@@ -9,7 +11,17 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
-        return 105;
+    	JsonArray players = request.getAsJsonObject().get("players").getAsJsonArray();
+    	int bet = 0;
+    	int i = 0;
+    	while(players.size() > i) {
+    		int betPlayer = players.get(i).getAsJsonObject().get("bet").getAsBigInteger().intValue();
+    		if(bet < betPlayer) {
+    			bet = betPlayer;
+    		}
+    		i++;
+    	}
+    	return bet + 1;
     }
 
     public static void showdown(JsonElement game) {
